30c3b42c7ce7ea287d4dcb85a43b4d90c205e39e
Alex Sander <alex.darienko@gmail.com>
2020-08-01 11:22:06 +0300

    Initial Commit

---
+++ HomeWork3.4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeWork3.4
+//
+//  Created by Alex Sander on 01.08.2020.
+//  Copyright © 2020 Alex Sander. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
